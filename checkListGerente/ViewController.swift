bb1b61a6fe2d075bfe2fe077977489784c07c924
Danilo Costa Tiago <danilotiago3@hotmail.com>
2024-03-26 15:59:47 -0300

    Initial Commit

---
+++ checkListGerente/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  checkListGerente
+//
+//  Created by Danilo Costa tiago on 26/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
