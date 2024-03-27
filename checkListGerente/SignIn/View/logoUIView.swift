513cf0984e5b3cbf40206f53858ba22792d13030
Danilo Costa Tiago <danilotiago3@hotmail.com>
2024-03-27 17:38:42 -0300

    adicionado regras de teclado na SignIn e SignUp

---
+++ checkListGerente/SignIn/View/logoUIView.swift
@@ -0,0 +1,20 @@
+//
+//  logoUIView.swift
+//  checkListGerente
+//
+//  Created by Danilo Costa tiago on 27/03/24.
+//
+
+import UIKit
+
+class logoUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
